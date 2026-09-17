3a713dc1adb43cc4b4d460c1ae5be879e557e7df
Jitheas A <jitheashwebstudio@gmail.com>
2026-09-17 16:02:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll("a");
-
-buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-        console.log("Navigation clicked");
-    });
-});
